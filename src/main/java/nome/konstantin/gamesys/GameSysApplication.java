7282e4ef8166e68c65930a4e6743a696c35d2263
Konstantin Tarletski <konstantin.tarletski@bigbank.ee>
2020-04-08 14:21:31 +0300

    Created new SpringBoot application

---
+++ src/main/java/nome/konstantin/gamesys/GameSysApplication.java
@@ -0,0 +1,13 @@
+package nome.konstantin.gamesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nome/konstantin/gamesys/GameSysApplicationTests.java
@@ -0,0 +1,13 @@
+package nome.konstantin.gamesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
