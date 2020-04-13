a5ca50790ba3724f1b01e03f8f88e0f998b70bf7
Konstantin Tarletski <konstantin.tarletski@bigbank.ee>
2020-04-13 11:11:59 +0300

    Writing more tests for Utils class

--- src/test/java/home/konstantin/gamesys/utils/UtilsTest.java
+++ src/test/java/home/konstantin/gamesys/utils/UtilsTest.java
@@ -1,20 +1,24 @@
-package home.konstantin.gamesys.service;
+package home.konstantin.gamesys.utils;
 
-import home.konstantin.gamesys.utils.Utils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.core.io.Resource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.ZoneId;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
 
 public class UtilsTest {
+
+    private final String TEXT = "Hello World";
 
     @Mock
     private Resource resource;
@@ -45,7 +49,16 @@ public class UtilsTest {
 
     @Test(expected = IOException.class)
     public void testResourceAsStringIOException() throws IOException {
+        when(resource.getInputStream()).thenThrow(IOException.class);
         assertNull(Utils.resourceAsString(resource));
+    }
+
+    @Test
+    public void testResourceAsStringSucess() throws IOException {
+        when(resource.getInputStream())
+            .thenReturn(new ByteArrayInputStream(TEXT.getBytes(StandardCharsets.UTF_8)));
+        var text = Utils.resourceAsString(resource);
+        assertEquals(text, TEXT);
     }
 
 }
